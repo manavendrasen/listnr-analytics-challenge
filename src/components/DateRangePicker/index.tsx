4b320c83ef2789698813819299b1d66b03c66684
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-07-12 02:20:13 +0530

    style: date picker

--- src/components/DateRangePicker/index.tsx
+++ src/components/DateRangePicker/index.tsx
@@ -1,22 +1,27 @@
 import { RangeDatePicker } from "react-google-flight-datepicker";
 import "react-google-flight-datepicker/dist/main.css";
 
+import "./DateRangePicker.css";
 interface Props {
 	startDate: Date;
 	endDate: Date;
-	onChange: (startDate: Date, endDate: Date) => void
+	onChange: (startDate: Date, endDate: Date) => void;
 }
 
 const DateRangePicker: React.FC<Props> = ({ startDate, endDate, onChange }) => {
 	return (
-		<RangeDatePicker
-			highlightToday
-			onChange={(startDate: Date, endDate: Date) => onChange(startDate, endDate)}
-			startDate={startDate}
-			endDate={endDate}
-			minDate={new Date(1900, 0, 1)}
-			maxDate={new Date(2100, 0, 1)}
-		/>
+		<div className='date-picker'>
+			<RangeDatePicker
+				highlightToday
+				onChange={(startDate: Date, endDate: Date) =>
+					onChange(startDate, endDate)
+				}
+				startDate={startDate}
+				endDate={endDate}
+				minDate={new Date(1900, 0, 1)}
+				maxDate={new Date(2100, 0, 1)}
+			/>
+		</div>
 	);
 };
 
