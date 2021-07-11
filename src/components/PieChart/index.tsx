8417ced2007972ce57de8d8a23e27359e60a19b8
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-07-12 02:21:41 +0530

    style: pie chart

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -1,5 +1,4 @@
-import { PieChart, Pie, Tooltip } from "recharts";
-
+import { PieChart, Pie, Legend, Tooltip, Cell } from "recharts";
 
 interface Props {
 	data: {
@@ -9,19 +8,26 @@ interface Props {
 }
 
 const PieChartFromData: React.FC<Props> = ({ data }) => {
+	const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#E53E3E", "#ED8936", "#9F7AEA", "#ED64A6"];
 	return (
-		<PieChart width={1000} height={400}>
+		<PieChart width={1000} height={500}>
 			<Pie
 				dataKey="value"
 				isAnimationActive={true}
 				data={data}
-				cx={200}
-				cy={200}
-				outerRadius={100}
-				fill="#234422"
-				label
-			/>
+				label={(label) => (`${(label.percent * 100).toFixed(0)}%`)}
+				cx="50%"
+				cy="50%"
+				labelLine={false}
+				outerRadius={150}
+				fill="#8884d8"
+			>
+				{data.map((entry, index) => (
+					<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+				))}
+			</Pie>
 			<Tooltip />
+			<Legend />
 		</PieChart>
 	);
 };
--- src/views/ChannelGroupingStats/index.tsx
+++ src/views/ChannelGroupingStats/index.tsx
@@ -1,15 +1,57 @@
-import React from 'react'
-import Results from '../../constants/result.json';
-import getPercentOfChannelGrouping from '../../utils/getPercentOfChannelGrouping'
-import PieChart from '../../components/PieChart'
+import React, { useEffect, useState } from "react";
+import Results from "../../constants/result.json";
+
+// components
+import DateRangePicker from '../../components/DateRangePicker'
+import PieChart from "../../components/PieChart";
+
+// utility
+import getPercentOfChannelGrouping from "../../utils/getPercentOfChannelGrouping";
+import getSessionsByStartEndDate from "../../utils/getSessionsByStartEndDate";
+
+// interfaces
+import { SessionsEntry } from '../../constants/models/sessions'
+
+// styles
+import "./ChannelGroupingStats.css";
 
 const ChannelGroupingStats = () => {
-	const chartDate = getPercentOfChannelGrouping(Results);
+	const [result, setResult] = useState<SessionsEntry[]>(Results)
+	const [date, setDate] = useState({
+		startDate: new Date("2017-09-01"),
+		endDate: new Date("2017-10-15"),
+	});
+
+	useEffect(() => {
+		const dataByDateRange = getSessionsByStartEndDate(
+			Results,
+			date.startDate,
+			date.endDate
+		);
+		setResult(dataByDateRange)
+	}, [date]);
+
+	const onChange = (startDate: Date, endDate: Date) => {
+		setDate({
+			startDate,
+			endDate,
+		});
+	};
+
+	const chartDate = getPercentOfChannelGrouping(result);
+
 	return (
 		<div>
-			<PieChart data={chartDate!} />
-		</div>
-	)
-}
+			<DateRangePicker
+				startDate={date.startDate}
+				endDate={date.endDate}
+				onChange={onChange}
+			/>
+			<section className="container">
+				<PieChart data={chartDate!} />
+			</section>
+		</div >
+	);
+};
 
-export default ChannelGroupingStats
+export default ChannelGroupingStats;
