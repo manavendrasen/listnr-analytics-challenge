6bb2302fbb88d550bffd83886de5bf57ef245bbc
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-07-12 16:28:38 +0530

    style: pie chart responsive

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -1,4 +1,4 @@
-import { PieChart, Pie, Legend, Tooltip, Cell } from "recharts";
+import { PieChart, Pie, Legend, Tooltip, Cell, ResponsiveContainer } from "recharts";
 
 interface Props {
 	data: {
@@ -10,25 +10,27 @@ interface Props {
 const PieChartFromData: React.FC<Props> = ({ data }) => {
 	const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#E53E3E", "#ED8936", "#9F7AEA", "#ED64A6"];
 	return (
-		<PieChart width={1000} height={500}>
-			<Pie
-				dataKey="value"
-				isAnimationActive={true}
-				data={data}
-				label={(label) => (`${(label.percent * 100).toFixed(0)}%`)}
-				cx="50%"
-				cy="50%"
-				labelLine={false}
-				outerRadius={150}
-				fill="#8884d8"
-			>
-				{data.map((entry, index) => (
-					<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-				))}
-			</Pie>
-			<Tooltip />
-			<Legend />
-		</PieChart>
+		<ResponsiveContainer width="100%" height={500}>
+			<PieChart width={1000} height={500}>
+				<Pie
+					dataKey="value"
+					isAnimationActive={true}
+					data={data}
+					label={(label) => (`${(label.percent * 100).toFixed(0)}%`)}
+					cx="50%"
+					cy="50%"
+					labelLine={false}
+					outerRadius={150}
+					fill="#8884d8"
+				>
+					{data.map((entry, index) => (
+						<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+					))}
+				</Pie>
+				<Tooltip />
+				<Legend />
+			</PieChart>
+		</ResponsiveContainer >
 	);
 };
 
