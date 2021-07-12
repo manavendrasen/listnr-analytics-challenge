73f6ec59c96fe4b21a72b7bce180926e9fe07954
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-07-12 16:28:55 +0530

    feat: add get data function

---
+++ src/utils/getData.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const URL = "https://gist.githubusercontent.com/manavendrasen/c83b7d0ef8cbb13b71b71f536494297b/raw/b9d8225a0e6dced8275a2a5af97325c2a5c31fb5/results.json"
+
+const getData = async () => {
+  let res = await axios(URL);
+  return res.data;
+};
+
+export default getData;
