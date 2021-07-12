f9707487bf4538c3333693eca9ec9ffe29d20485
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-07-12 18:37:06 +0530

    refactor: add getData url to constants

---
+++ src/constants/api/stats.ts
@@ -0,0 +1,1 @@
+export const GET_FORMATTED_JSON_URL = "https://gist.githubusercontent.com/manavendrasen/c83b7d0ef8cbb13b71b71f536494297b/raw/b9d8225a0e6dced8275a2a5af97325c2a5c31fb5/results.json"
--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
 // url endpoint
-const URL = "https://gist.githubusercontent.com/manavendrasen/c83b7d0ef8cbb13b71b71f536494297b/raw/b9d8225a0e6dced8275a2a5af97325c2a5c31fb5/results.json"
-
+import {GET_FORMATTED_JSON_URL} from '../constants/api/stats'
 // load data from url
 const getData = async () => {
-  let res = await axios(URL);
+  let res = await axios(GET_FORMATTED_JSON_URL);
   return res.data;
 };
 
